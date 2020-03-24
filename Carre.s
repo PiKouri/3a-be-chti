25ed9d8fe707d9aace4285a95e6ac60b409801c5
Jean-Rémy HOK <hok@etud.insa-toulouse.fr>
2020-03-24 16:05:06 +0100

    Factorisation de quelques instructions

--- Carre.s
+++ Carre.s
@@ -17,27 +17,22 @@ timer_callback	proc
 	
 	ldr  	r0 ,=etat	;On recupère l'addresse de etat
 	ldr  	r2 ,[r0]	;Puis son contenu
+	ldr	r3, =GPIOB_BSRR
 	cbnz 	r2 ,cas_1	;Si PB1 n'est pas à 0, on vas à cas_1
-	cbz 	r2 ,cas_0	;Si PB1 est à 0, on vas à cas_0
-	bx	lr		; dernière instruction de la fonction
-	endp
-	
-cas_1	proc
-; mise a zero de PB1
-	ldr	r3, =GPIOB_BSRR
-	mov	r1, #0x00020000
-	str	r1, [r3]
-	mov 	r2, #0x00000000
-	str	r2, [r0]
-	bx	lr		; dernière instruction de la fonction
-	endp
-	
-cas_0	proc
-	; mise a 1 de PB1
-	ldr	r3, =GPIOB_BSRR
+
+; mise a 1 de PB1
 	mov	r1, #0x00000002
 	str	r1, [r3]
 	mov 	r2, #0x00000001
+	b	cas_0
+
+cas_1	
+; mise a zero de PB1
+	mov	r1, #0x00020000
+	str	r1, [r3]
+	mov 	r2, #0x00000000
+
+cas_0	
 	str	r2, [r0]
 	bx	lr		; dernière instruction de la fonction
 	endp
