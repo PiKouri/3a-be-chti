38a5e73627f7995367186c1b08f73280f6171f6c
Jean-Rémy HOK <hok@etud.insa-toulouse.fr>
2020-03-24 18:45:21 +0100

    Calcul cos² + sin²

--- etape2.s
+++ etape2.s
@@ -6,13 +6,18 @@ GPIOB_BSRR	equ	0x40010C10	; Bit Set/Reset register
 	area  	moncode, code, readonly
 	extern	TabCos
 	extern	TabSin
+	export	calcul
 
 calcul	proc
 	
 	ldr	r2, =TabCos
 	ldr	r3, =TabSin
-	ldrsh	r1, [r2, #4]
-	bx	lr;
+	add	r0, r0
+	ldrsh	r1, [r2, r0]
+	ldrsh	r12, [r3, r0]
+	mul	r1, r1, r1
+	mla	r0, r12, r12, r1 	
+	bx	lr
 	endp
 ;
 	end
--- principal_v2.c
+++ principal_v2.c
@@ -1,9 +1,10 @@
 # include "gassp72.h"
 
-void calcul(void);
+void calcul(short i);
 
 int main(void)
 {
+calcul(0);
 while	(1)
 	{
 	}
