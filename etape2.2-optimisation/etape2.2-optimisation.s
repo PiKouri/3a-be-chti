dc96df4131d2ed83cb8c87c160acc7444f219cc2
Jean-Rémy HOK <hok@etud.insa-toulouse.fr>
2020-03-31 17:27:50 +0200

    Optimisation DFT 3

--- etape2.2-optimisation/etape2.2-optimisation.s
+++ etape2.2-optimisation/etape2.2-optimisation.s
@@ -6,7 +6,7 @@
 	extern	TabCos
 	extern	TabSin
 
-N	EQU	64
+N	EQU	128
 
 M2	proc
 	push	{lr, r4, r5, r0, r1}
@@ -28,35 +28,33 @@ M2	proc
 	endp
 
 calcul	proc
-	;r0 est l'adresse de base du signal
+	;r0 adresse de l'element x(i) (initialement adresse de base du signal)
 	;r1 est la valeur de k
 	;r2 est l'adresse de la table de cos
-	;r3 est la valeur de n
+	;r3 est adresse du tableau de signal
 	;r4 vaut ik
-	;r5 est le compteur de la boucle for
 	;r6 est cos(ik)
 	;r7 est x(i)
 	;r12 est la valeur de la somme
-	push 	{r4, r5, r6, r7}
+	push 	{r4, r6, r7}
 	mov	r3, #N
-	mov 	r5, #0
+	add	r3, r0
 	mov	r12, #0
 	mov	r4, #0
 	
 Boucle
 	
-	ldrsh	r7,[r0,r5, LSL #1] ; r7 = x(i) (adresse = r0 + r5 * 2) | r7 format 1.15
+	ldrsh	r7, [r0], #2 ; r7 = x(i) (adresse = r0) | puis r0 = r0+2 | r7 format 1.15
 	ldrsh	r6, [r2, r4, LSL #1] ; r6 = cos(ik) (adresse = r2 + r4 * 2) | r7 format 1.15
 	
 	mla	r12, r6, r7, r12
 		
 	add	r4, r4, r1 ;calcul de ik
-	add 	r5, #1 ;On incrémente la valeur contenue dans r12
-	and	r4, #63 ;modulo N=64
-	cmp 	r3, r5
+	and	r4, #63 ; modulo N=64
+	cmp 	r3, r0 ; 
 	bne 	Boucle ;Si r5!=r3, on boucle
 	
-	pop 	{r4, r5, r6, r7}
+	pop 	{r4, r6, r7}
 	mov 	r0, r12 ; r0 format 2.30
 
 	bx	lr
